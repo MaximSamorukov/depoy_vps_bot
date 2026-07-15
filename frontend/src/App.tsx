0cfc048628ea9bb61b1bd6fadc9ac575bc6f2039
maksim.samorukov <maxim.samorukov@gmail.com>
2026-07-15 23:17:40 +0300

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import styles from './App.module.scss'
 function App() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>frontend!!!!!</h1>
+      <h1 className={styles.title}>frontend!!!!!!</h1>
     </div>
   )
 }
