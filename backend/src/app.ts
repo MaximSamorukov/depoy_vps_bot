7f6a86a54cf1d13db761bd4fe9a0529095a83a0b
maksim.samorukov <maxim.samorukov@gmail.com>
2026-07-17 20:54:49 +0300

    test

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,7 +15,14 @@ export const createApp = () => {
   const controller = new MessagesController(service);
 
   const app = express();
-  app.use(cors());
+  app.use(
+    cors({
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+      credentials: false,
+    })
+  );
   app.use(express.json());
 
   const swaggerSpec = swaggerJSDoc(swaggerOptions);
